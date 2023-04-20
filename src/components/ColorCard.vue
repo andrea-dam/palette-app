c37d5f5b27b7b26167504207ddf60dcfa63b6944
Andrea Damiani <andrea.dam90@gmail.com>
2023-04-20 13:11:13 +0200

    Implementata funzionalità copia nella clipboard

--- src/components/ColorCard.vue
+++ src/components/ColorCard.vue
@@ -1,14 +1,16 @@
 <template>
-    <div class="relative flex h-full w-48 flex-col">
+    <div class="group relative flex h-full w-48 flex-col">
         <div :id="`card${cardNumber}`" class="h-full rounded border-2 drop-shadow-sm"></div>
         <div>
             <ColorSlider :card-number="cardNumber" sliderNumber="1" attributo="H" @update-value="value => (hue = value)" max="360" />
             <ColorSlider :card-number="cardNumber" sliderNumber="2" attributo="S" @update-value="value => (saturation = value)" max="100" />
             <ColorSlider :card-number="cardNumber" sliderNumber="3" attributo="L" @update-value="value => (lightness = value)" max="100" />
         </div>
-        <div class="absolute right-5 top-5 h-12 w-12 rounded bg-gray-500">
-            <Icon icon="mdi:clipboard-multiple-outline" class="text-3xl text-gray-500" />
-        </div>
+        <button
+            @click="copy(result)"
+            class="absolute right-5 top-5 hidden h-10 w-10 items-center justify-center rounded bg-gray-400/70 text-white hover:bg-blue-200 hover:text-gray-900 group-hover:flex">
+            <Icon icon="mdi:clipboard-multiple-outline" class="text-3xl" />
+        </button>
     </div>
 </template>
 
@@ -19,22 +21,19 @@ import ColorSlider from "../components/ColorSlider.vue";
 
 const props = defineProps(["cardNumber"]);
 
-const hue = ref(180);
-const saturation = ref(50);
+const hue = useStorage(`card${props.cardNumber}` + "slider1", 180);
+const saturation = useStorage(`card${props.cardNumber}` + "slider2", 50);
 let saturationPercentage = `${saturation.value}%`;
-const lightness = ref(50);
+const lightness = useStorage(`card${props.cardNumber}` + "slider3", 50);
 let lightnessPercentage = `${lightness.value}%`;
 
-const storedHue = useStorage(`card${props.cardNumber}` + "slider1", 180);
-const storedSaturation = useStorage(`card${props.cardNumber}` + "slider2", 50);
-const storedLightness = useStorage(`card${props.cardNumber}` + "slider3", 50);
+// const storedHue = useStorage(`card${props.cardNumber}` + "slider1", 180);
+// const storedSaturation = useStorage(`card${props.cardNumber}` + "slider2", 50);
+// const storedLightness = useStorage(`card${props.cardNumber}` + "slider3", 50);
 
-const css = useStyleTag(
-    `#card${props.cardNumber} { background-color: hsl(${storedHue.value}, ${storedSaturation.value}%, ${storedLightness.value}%) }`,
-    {
-        id: `id${props.cardNumber}`,
-    }
-);
+const css = useStyleTag(`#card${props.cardNumber} { background-color: hsl(${hue.value}, ${saturation.value}%, ${lightness.value}%) }`, {
+    id: `id${props.cardNumber}`,
+});
 
 watch(hue, newValue => {
     if (newValue) {
@@ -68,4 +67,10 @@ watch(lightness, newValue => {
         );
     }
 });
+
+const result = ref(`hsl(${hue.value}, ${saturationPercentage}, ${lightnessPercentage})`);
+
+console.log(result.value);
+
+const { text, copy, copied, isSupported } = useClipboard({ result });
 </script>
