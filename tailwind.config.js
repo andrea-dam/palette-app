14dbaf62f0f62c9aec86ded803ad02676cdae4ba
Andrea Damiani <andrea.dam90@gmail.com>
2023-04-21 16:13:02 +0200

    Creato file per deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     darkMode: "class",
     theme: {
+        fontFamily: {
+            sans: ['Inter', 'sans-serif'],
+        },
         extend: {
             gridTemplateRows: {
                 10: "repeat(10, minmax(0, 1fr))",
