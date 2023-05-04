855147b07a1b702392231a56ffec1b61ec08d5b3
Andrea Damiani <andrea.dam90@gmail.com>
2023-05-04 15:00:48 +0200

    Avvio implementazione logica conversione da hsl a rgb

--- src/components/ColorCard.vue
+++ src/components/ColorCard.vue
@@ -39,13 +39,14 @@
 </template>
 
 <script setup>
-import { ref, watch } from "vue";
+import { computed, ref, watch } from "vue";
 import { useStyleTag, useStorage, useClipboard } from "@vueuse/core";
 import ColorSlider from "../components/ColorSlider.vue";
 
 const props = defineProps({
     cardNumber: Number,
     selectedPalette: Number,
+    convertToRgb: Boolean,
 });
 
 const hue = useStorage(`palette${props.selectedPalette}-card${props.cardNumber}-slider1`, (Math.random() * (360 - 0) + 0).toFixed(1));
@@ -96,7 +97,59 @@ watch(lightness, newValue => {
     }
 });
 
-const result = ref(`hsl(${hue.value}, ${saturationPercentage}, ${lightnessPercentage})`);
+// Sezione Conversione
+
+const saturationForConversion = computed(() => {
+    return saturation.value / 100;
+});
+
+const c = computed(() => {
+    return (1 - (lightness.value * 2 - 1)) * saturationForConversion.value;
+});
+
+const x = computed(() => {
+    return c * (1 - (((H / 60) % 2) - 1));
+});
+
+const m = computed(() => {
+    return lightness.value - c / 2;
+});
+
+let r1, g1, b1;
+
+if (hue.value >= 0 && hue.value < 60) {
+    r1 = c;
+    g1 = x;
+    b1 = 0;
+} else if (hue.value >= 60 && hue.value < 120) {
+    r1 = x;
+    g1 = c;
+    b1 = 0;
+} else if (hue.value >= 120 && hue.value < 180) {
+    r1 = 0;
+    g1 = c;
+    b1 = x;
+} else if (hue.value >= 180 && hue.value < 240) {
+    r1 = 0;
+    g1 = x;
+    b1 = c;
+} else if (hue.value >= 240 && hue.value < 300) {
+    r1 = x;
+    g1 = 0;
+    b1 = c;
+} else if (hue.value >= 300 && hue.value <= 360) {
+    r1 = c;
+    g1 = 0;
+    b1 = x;
+}
+
+let r = (r1 + m) * 255;
+let g = (g1 + m) * 255;
+let b = (b1 + m) * 255;
+
+const result = computed(() => {
+    return props.convertToRgb ? `rgb(${r}, ${g}, ${b})` : `hsl(${hue.value}, ${saturationPercentage}, ${lightnessPercentage})`;
+});
 
 const { copy, copied } = useClipboard({ result });
 </script>
--- src/components/ColorPalette.vue
+++ src/components/ColorPalette.vue
@@ -2,7 +2,7 @@
     <!-- Area Card -->
     <main class="row-span-8 bg-main-area pb-5 pt-6 duration-1000 dark:bg-slate-900">
         <CardContainer v-if="selectedPalette === 1">
-            <ColorCard v-for="card in openCards" :key="card" :card-number="card" :selected-palette="1" />
+            <ColorCard v-for="card in openCards" :key="card" :card-number="card" :selected-palette="1" :convert-to-rgb="isRGB" />
         </CardContainer>
         <CardContainer v-else-if="selectedPalette === 2">
             <ColorCard v-for="card in openCards" :key="card" :card-number="card" :selected-palette="2" />
@@ -34,22 +34,40 @@
     </main>
 
     <!-- Barra Inferiore -->
-    <footer class="row-span-1 grid grid-cols-2 items-center justify-center space-x-4 bg-header-footer dark:bg-slate-800">
-        <!-- Pulsante Meno -->
-        <div class="flex items-center justify-between space-x-2 pl-6">
-            <h3>{{ name }}</h3>
-            <SignButton v-show="openCards > 1" @click="changeCards('-')" icon="ic:round-minus" />
+    <footer class="row-span-1 flex items-center justify-between bg-header-footer px-6 dark:bg-slate-800">
+        <!-- Nome Palette Selezionata -->
+        <h3>{{ name }}</h3>
+
+        <!-- Pulsanti Modifica Carte -->
+        <div class="grid grid-cols-2 items-center gap-2">
+            <!-- Pulsante Meno -->
+            <div>
+                <SignButton v-show="openCards > 1" @click="changeCards('-')" icon="ic:round-minus" />
+            </div>
+
+            <!-- Pulsante Più -->
+            <div>
+                <SignButton v-show="openCards < 5" @click="changeCards('+')" icon="ic:round-plus" />
+            </div>
         </div>
 
-        <!-- Pulsante Più -->
-        <div class="flex items-center space-x-2">
-            <SignButton v-show="openCards < 5" @click="changeCards('+')" icon="ic:round-plus" />
+        <!-- Selettore HSL/RGB -->
+        <div class="flex content-end items-center space-x-4">
+            <p>HSL</p>
+            <input
+                class="checked:bg-primary checked:after:bg-primary checked:focus:border-primary checked:focus:bg-primary dark:checked:bg-primary dark:checked:after:bg-primary mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
+                type="checkbox"
+                role="switch"
+                @click="toggleRGB()"
+                :checked="isRGB" />
+            <p>RGB</p>
         </div>
     </footer>
 </template>
 
 <script setup>
-import { useStorage } from "@vueuse/core";
+import { ref } from "vue";
+import { useStorage, useToggle } from "@vueuse/core";
 
 import CardContainer from "./CardContainer.vue";
 import ColorCard from "./ColorCard.vue";
@@ -59,6 +77,9 @@ const props = defineProps({
     selectedPalette: Number,
     name: String,
 });
+
+const isRGB = ref(false);
+const toggleRGB = useToggle(isRGB);
 
 const openCards = useStorage(`carte-aperte-palette${props.selectedPalette}`, 1);
 
